798b7d2459a282202287faab68707b06aeb6efb3
meisun227 <meysunlee@gmail.com>
2024-02-22 13:32:57 +0200

    add loading status handler

--- src/Components/ChatBot/ChatBotContainer.jsx
+++ src/Components/ChatBot/ChatBotContainer.jsx
@@ -5,23 +5,35 @@ import InputBox from './InputBox'
 
 const ChatBotContainer = ({ isVisible }) => {
     const [chatSet, setChatSet] = useState([])
-    
+    const [loading, setLoading] = useState(true)
+
     const handleSubmit = async (question) => {
         try {
+            setLoading(true)
             const res = await axios.post('https://chatbot-backend-p7tt.onrender.com/ask', { question })
             setChatSet((prevSet) => [
                 ...prevSet,
                 { question, response: res.data.answer },
             ])
+            setLoading(false)
         } catch (error) {
             console.error('Error fetching answer:', error)
+        }
+        finally {
+            setLoading(false) // Ensure loading is set to false after response is received
         }
     }
 
     return (
         <div  className={`fixed bottom-0 right-56 mr-4 mb-4 bg-slate-400/70  px-2 py-2 rounded-lg ${isVisible ? 'block' : 'hidden'}`}>
-            <BotMessage chatSet={chatSet} />
-            <InputBox onSubmit={handleSubmit} />
+            <div>
+                <BotMessage chatSet={chatSet} />
+                {loading && <div >Loading...</div>} 
+                <InputBox onSubmit={handleSubmit} />
+             
+            </div>
+     
+     
         </div>
     )
 }
